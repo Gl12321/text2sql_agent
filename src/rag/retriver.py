8f2509fdf6dc68d5e33a03543fe99c764c19425a
Stanislav Rogov <stanislavrogov68@gmail.com>
2026-01-18 14:17:58 +0300

    rewrote retriever with LangChain

--- src/rag/retriver.py
+++ src/rag/retriver.py
@@ -1,46 +1,32 @@
-import chromadb
 from typing import Any
-from src.core.logger import setup_logger
+from langchain_core.retrievers import BaseRetriever
+from langchain_core.documents import Document
+from langchain_core.callbacks import CallbackManagerForRetrieverRun
 
 
-logger = setup_logger("retriver")
+class TableRetriever(BaseRetriever):
+    collections: Any
+    embedder: Any
+    db_id: str
+    top_k: int = 2
 
-class Retriver:
-    def __init__(self, path = "/home/stanislav/Enterprice_SQL_agent/data"):
-        self.client = chromadb.PersistentClient(path=path)
-        self.table_collection = self.client.get_or_create_collection(
-            name="tables",
-            metadata={"hnsw:space": "cosine"}
-        )
-        # ДОПИСАТЬ ПОИСК ПО СХЕМАМ С РЕРАНКЕРОМ
-        self.shema_collection = self.client.get_or_create_collection(
-            name="schemes",
-            metadata={"hnsw:space": "cosine"}
-        )
+    def _get_relevant_documents(self, query: str, *, run_manager: CallbackManagerForRetrieverRun):
+        query_embedding = self.embedder(query)
 
-    def add_tables(
-            self,
-            db_id, table_names: list[str],
-            documents_of_ddl: list[str],
-            embeddings: list[list[float]],
-            metadatas: list[dict]
-    ):
-        ids = [f"{db_id}.{name}" for name in table_names]
-        self.table_collection.upsert(
-            ids=ids,
-            embeddings=embeddings,
-            documents=documents_of_ddl,
-            metadatas=metadatas
-        )
-        logger.info(f"Added tables of schema: {db_id}")
+        filter = {"db_id": self.db_id}
 
-    def search_tables(self, query_embedding, db_id, top_k=5):
-        search_filter = {"db_id": db_id}
-
-        results = self.table_collection.query(
-            query_embeddings=[query_embedding],
-            n_results=top_k,
-            where=search_filter
+        results = self.collection.query(
+            query_embedding=[query_embedding],
+            n_results=self.top_k,
+            where=filter
         )
 
-        return results["documents"][0]
+        documents = []
+        for i in range(len(results["ids"][0])):
+            doc = Document(
+                page_content=results["ids"][0][i],
+                metadata=results["metadata"][0][i]
+            )
+            documents.append(doc)
+
+        return documents
--- tests/t_full_RAG_test.py
+++ tests/t_full_RAG_test.py
@@ -1,36 +1,34 @@
 import asyncio
 from src.database.schema_parser import SchemaParser
 from src.rag.cataloger import SchemaCataloger
-from src.rag.retriver import Retriver
+from src.rag.retriver import TableRetriever
 from src.rag.embedder import TableEmbedder
 from src.core.logger import setup_logger
 
 logger = setup_logger("RAG_Test")
 
+
 async def rag_test():
     parser = SchemaParser()
     cataloger = SchemaCataloger()
-    retriver = Retriver()
     embedder = TableEmbedder()
 
     logger.info("Check data migration")
 
+    await cataloger.index_all_schemas()
     all_schemas = await parser.get_all_schemas()
+
     if not all_schemas:
         return
 
     postgres_stats = {}
     for schema in all_schemas:
         tables = await parser.get_ddl_of_schema(schema)
-
         postgres_stats[schema] = list(tables.keys())
 
-    await cataloger.index_all_schemas()
-
     for schema, tables_list in postgres_stats.items():
-        results = retriver.table_collection.get(where={"db_id": schema})
+        results = cataloger.table_collection.get(where={"db_id": schema})
         actual_count = len(results['ids'])
-        # Сравнение через длину сохраненного списка
         expected_tables = len(tables_list)
 
         if actual_count != expected_tables:
@@ -41,24 +39,24 @@ async def rag_test():
     logger.info("Check semantic search")
 
     test_db = "college_2"
-
-    logger.info(f"tables of schema {test_db}: {postgres_stats.get(test_db, [])}")
-    
     test_query = "how match buildings in department and classrooms"
-    query_vector = embedder.get_embeddings([test_query])[0]
 
-    search_results = retriver.search_tables(
-        query_embedding=query_vector,
+    retriever = TableRetriever(
+        collection=cataloger.table_collection,
+        embedder=embedder,
         db_id=test_db,
         top_k=2
     )
 
+    search_results = retriever.invoke(test_query)
+
     if search_results:
         logger.info(f"Search for '{test_db}' returned {len(search_results)} documents")
         for i, doc in enumerate(search_results):
-            logger.info(f"Top-{i+1} result snippet: {doc[:150]}...")
+            logger.info(f"Top-{i + 1} result snippet: {doc.page_content[:150]}...")
     else:
         logger.error(f"Search returned NO results for schema '{test_db}'")
+
 
 if __name__ == "__main__":
     asyncio.run(rag_test())
