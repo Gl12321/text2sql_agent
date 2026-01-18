c3b877a036b26b2681671dab0d34b5d7ff9aaf9f
Stanislav Rogov <stanislavrogov68@gmail.com>
2026-01-18 17:11:47 +0300

    add llm pack

---
+++ src/llm/chains.py
@@ -0,0 +1,43 @@
+from typing import Any
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.documents import Document
+
+
+def _parse_retrieval_output(docs: list[Document]):
+    tables = [d.metadata.get("table_name") for d in docs]
+
+    nested_cols = [d.metadata.get("column_names", "").split(",") for d in docs]
+    columns = list(set(col for sublist in nested_cols for col in sublist if col))
+
+    return {
+        "docs": docs,
+        "tables": tables,
+        "columns": columns
+    }
+
+
+def create_sql_chain(llm_wrapper, retriever, prompt_manager):
+    retrieval_chain = (
+            retriever
+            | RunnableLambda(_parse_retrieval_output)
+    )
+
+    full_chain = (
+            {
+                "data": retrieval_chain,
+                "question": RunnablePassthrough()
+            }
+            | RunnableLambda(lambda x: {
+        "prompt": prompt_manager.build_sql_prompt(x["question"], x["data"]["docs"]),
+        "tables": x["data"]["tables"],
+        "columns": x["data"]["columns"]
+    })
+            | RunnableLambda(lambda x: llm_wrapper.get_chain(
+        tables=x["tables"],
+        columns=x["columns"]
+    ).invoke(x["prompt"]))
+            | StrOutputParser()
+    )
+
+    return full_chain
